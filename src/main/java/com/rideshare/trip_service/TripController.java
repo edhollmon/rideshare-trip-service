336fd021933eb30b1c3a5d5c847c9a388df9289a
Edward Hollmon <edhollmon@gmail.com>
2025-05-02 11:43:46 -0700

    Refactore routes

--- src/main/java/com/rideshare/trip_service/TripController.java
+++ src/main/java/com/rideshare/trip_service/TripController.java
@@ -5,49 +5,49 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/trip")
 public class TripController {
 
-    @PostMapping("/requestTrip")
+    @PostMapping("/request-trip")
     public String requestTrip() {
         System.out.println("Trip requested");
         return "Trip requested";
     }
 
-    @PostMapping("/startMatching")
+    @PostMapping("/start-matching")
     public String startMatching() {
         System.out.println("Matching started");
         return "Matching started";
     }
 
-    @PostMapping("/driverAccepts")
+    @PostMapping("/driver-accepts")
     public String driverAccepts() {
         System.out.println("Driver accepted the trip");
         return "Driver accepted the trip";
     }
 
-    @PostMapping("/driverArrives")
+    @PostMapping("/driver-arrives")
     public String driverArrives() {
         System.out.println("Driver arrived at the pickup location");
         return "Driver arrived at the pickup location";
     }
 
-    @PostMapping("/tripStart")
+    @PostMapping("/trip-start")
     public String tripStart() {
         System.out.println("Trip started");
         return "Trip started";
     }
 
-    @PostMapping("/tripCompleted")
+    @PostMapping("/trip-completed")
     public String tripCompleted() {
         System.out.println("Trip completed");
         return "Trip completed";
     }
 
-    @PostMapping("/riderCancelTrip")
+    @PostMapping("/rider-cancel-trip")
     public String riderCancelTrip() {
         System.out.println("Rider canceled the trip");
         return "Rider canceled the trip";
     }
 
-    @PostMapping("/driverCancelTrip")
+    @PostMapping("/driver-cancel-trip")
     public String driverCancelTrip() {
         System.out.println("Driver canceled the trip");
         return "Driver canceled the trip";
